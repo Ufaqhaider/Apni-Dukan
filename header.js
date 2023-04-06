83da57bf4f58b430b8cb580174544079826063ee
Ufaqhaider <74580561+Ufaqhaider@users.noreply.github.com>
2023-04-07 01:47:20 +0530

    Update header.js

--- header.js
+++ header.js
@@ -2,7 +2,7 @@ nav=document.getElementById('nav')
 
 nav.innerHTML= `
 <nav class="navbar navbar-expand-lg ">
-<a class="navbar-brand" href="#"><img src="assets/Screenshot_from_2023-04-06_17-36-48-removebg-preview (1).png" class="img-fluid"></a>
+<a class="navbar-brand" href="index.html"><img src="assets/Screenshot_from_2023-04-06_17-36-48-removebg-preview (1).png" class="img-fluid"></a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <i class="fa-solid fa-bars" style="color: #ffffff;"></i>
 </button>
